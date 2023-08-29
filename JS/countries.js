e3c3dad01121eef02244fe430f98e37b06188e86
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-29 19:26:04 -0300

    Added more EventListeners and forEach

--- JS/countries.js
+++ JS/countries.js
@@ -21,3 +21,50 @@ exchageIcon.addEventListener("click", () => {
   selectTag[0].value = selectTag[1].value;
   selectTag[1].value = tempLang;
 });
+
+fromText.addEventListener("keyup", () => {
+  if (!fromText.value) {
+    toText.value = "";
+  }
+});
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+  if (!text) return;
+  toText.setAttribute("placeholder", "Translating...");
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl).then(res => res.json()).then(data => {
+    toText.value = data.responseData.translatedText;
+    data.matches.forEach(data => {
+      if (data.id === 0) {
+        toText.value = data.translation;
+      }
+    });
+    toText.setAttribute("placeholder", "Translation");
+  });
+});
+
+icons.forEach(icon => {
+  icon.addEventListener("click", ({ target }) => {
+    if (!fromText.value || !toText.value) return;
+    if (target.classList.contains("fa-copy")) {
+      if (target.id == "from") {
+        navigator.clipboard.writeText(fromText.value);
+      } else {
+        navigator.clipboard.writeText(toText.value);
+      }
+    } else {
+      let utterance;
+      if (target.id == "from") {
+        utterance = new SpeechSynthesisUtterance(fromText.value);
+        utterance.lang = selectTag[0].value;
+      } else {
+        utterance = new SpeechSynthesisUtterance(toText.value);
+        utterance.lang = selectTag[1].value;
+      }
+      speechSynthesis.speak(utterance);
+    }
+  });
+});
